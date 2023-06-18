8f860b5c0ae37ae05edf069268466d0ff916c9a3
ch <s2123@e-mirim.hs.kr>
2023-06-18 15:24:31 +0900

    level변경 시 보스의 공격 타입 변경

--- src/BossEnemy.java
+++ src/BossEnemy.java
@@ -3,6 +3,7 @@ import java.awt.*;
 public class BossEnemy extends Enemy {
     int cnt; // 움직임 타이밍
     int mode; // 보스 이동 방향
+    int level;  // 챕터
     int a = -1; // 앞뒤 이동 횟수
     boolean movingForward; // 보스 앞으로 이동중인가?
     int distanceMoved; // 보스가 앞, 뒤로 이동한 거리
@@ -12,18 +13,49 @@ public class BossEnemy extends Enemy {
         hp = 1000 + 300 * level;
         mode = 4;
         cnt = 1;
-        movingForward = true;
-        distanceMoved = 0;
+        this.level = level;
+        if(level == 2) {
+            movingForward = true;
+            distanceMoved = 0;
+        }
     }
 
-
-    // 만약 1 챕터라면?
-    // ... 작성
-
-    // 만약 2 챕터라면?
-    // ...  작성
-
     public void move() {
+        if(level==1) move1();
+        else if(level==2) move2();
+    }
+
+    // 1 챕터 보스 움직임
+    public void move1() {
+        if(x>600) {
+            x-=5;
+        }
+        cnt++;
+
+        if(cnt%300==0) {
+            cnt = 1;
+            a++;
+            mode = a%4;
+        }
+
+        switch (mode){
+            case 4 :
+                break;
+            case 0 :    // 위로 이동
+            case 1 :
+                if(y>60) y-=3;
+                if(cnt%60==0) mode=4;
+                break;
+            case 2 :    // 아래로 이동
+            case 3 :
+                if(y<500) y+=3;
+                if(cnt%60==0) mode=4;
+                break;
+        }
+    }
+
+    // 2챕터 보스 움직임
+    public void move2() {
         if (movingForward) { // true면 앞으로
             if (x > 300) {
                 x -= 7;
